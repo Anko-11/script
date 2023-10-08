264382fd9edffcd6ebc54cb16413eafffcd52275
yuzi <125455495+Anko-11@users.noreply.github.com>
2023-10-08 18:17:52 +0800

    Delete 脚本 directory

--- 脚本/python脚本/ip合法性检验（正则表达式）.py
+++
@@ -1,7 +0,0 @@
-import re
-ip = input("输入ip：")
-parts = ip.split('.')
-if re.match(r"^(\d{1,3}\.){3}\d{1,3}$", ip) and all(0 <= int(part) <= 255 for part in parts):
-    print("合法ip")
-else:
-    print("不合法ip")
