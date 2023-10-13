04b2c7efadfda4f1bcb15cfede7b631605d655f5
yuzi <125455495+Anko-11@users.noreply.github.com>
2023-10-13 17:40:14 +0800

    Delete shell脚本 directory

--- shell脚本/user_del.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in $(seq $2)
-do
-	userdel $1$i
-done
-
--- shell脚本/while小脚本/while3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tail -n +2 grade.txt |while read id name subject grade
-do
-	if (($grade>70));then
-		echo $name $subject $grade
-	fi
-done 
